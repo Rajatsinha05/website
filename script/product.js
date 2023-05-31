3aad5314be028ad83beebaead605a2a6c3799fa6
jara <jaramangukiya@gmail.com>
2023-05-31 18:04:34 +0530

    prodct page qty

--- script/product.js
+++ script/product.js
@@ -1,47 +1,57 @@
 import nav from "../components/Nav.js";
 
-let show=(data)=>{
-    console.log(data);
-    document.getElementById("products").innerHTML=""
-data.map((ele)=>{
-let img=document.createElement("img")
-img.src=ele.image
-let title=document.createElement("h3")
-title.innerHTML=ele.title
-let price=document.createElement("p")
-price.innerHTML=ele.price*5
-let category=document.createElement("p")
-category.innerHTML=ele.category
-let rating=document.createElement("p")
-rating.innerHTML=ele.rating.rate
-let btn=document.createElement("button")
-btn.innerHTML="Add to cart"
-let btn2=document.createElement("button")
-btn2.innerHTML="Buy Now"
-let divbtn=document.createElement("div")
-divbtn.append(btn,btn2)
-// divbtn.setAttributes("class","btns")
-let div=document.createElement("div") 
-div.append(img,title,category,price,rating,divbtn)  
-document.getElementById("products").append(div)
+let show = (data) => {
+  console.log(data);
+  document.getElementById("products").innerHTML = "";
+  data.map((ele) => {
+    let img = document.createElement("img");
+    img.src = ele.image;
+    let title = document.createElement("h3");
+    title.innerHTML = ele.title;
+    let price = document.createElement("p");
+    price.innerHTML = ele.price * 5;
+    let category = document.createElement("p");
+    category.innerHTML = ele.category;
+    let rating = document.createElement("p");
+    rating.innerHTML = ele.rating.rate;
+    let btn = document.createElement("button");
+    btn.innerHTML = "Add to cart";
+    let btn2 = document.createElement("button");
+    btn2.innerHTML = "Buy Now";
+    let divbtn = document.createElement("div");
+    divbtn.append(btn, btn2);
+    // divbtn.setAttributes("class","btns")
+    let div = document.createElement("div");
+    div.append(img, title, category, price, rating, divbtn);
+    document.getElementById("products").append(div);
 
-btn.addEventListener("click",()=>{
-    let cart=JSON.parse(localStorage.getItem("cart"))||[]
-    console.log(ele);
-    cart.push(ele);
-    localStorage.setItem("cart",JSON.stringify(cart));
+    btn.addEventListener("click", () => {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-} )
+      let exist = false;
 
+      cart.map((item) => {
+        if (item.id == ele.id) {
+            console.log(item);
+          cart.push({ ...item, qty: item.qty++ });
+          exist = true;
+        }
+      });
 
-})
-}
+      if (exist == false) {
+        cart.push({ ...ele, qty: 1 });
+        localStorage.setItem("cart", JSON.stringify(cart));
+        window.location.reload();
+      }
+    });
+  });
+};
 
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((data) => show(data));
 
+document.getElementById("nav").innerHTML = nav();
 
-
-fetch('https://fakestoreapi.com/products')
-.then((response)=>response.json())
-.then((data)=>show(data));
-
-document.getElementById("nav").innerHTML=nav()
+let arr=[3,5,6,6,5,6,7,6,7,6]
+console.log([...arr,45,6,8,789,8,8]);
